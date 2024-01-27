a5e774c0d217c083bc781b40d47f0f857a5e2eae
Chase <chase.lindsay1@gmail.com>
2024-01-27 10:05:39 -0500

    Added pricing logic to bookings model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -6,7 +6,26 @@ class Booking < ApplicationRecord
   validates :hours_requested, numericality: { only_integer: true, greater_than_or_equal_to: 2, less_than_or_equal_to: 8 }
   validate :date_of_service_cannot_be_in_the_past
 
+  BASE_CHARGE = 20
+  DOG_CHARGE = 10
+  CAT_CHARGE = 5
+
+  def cost
+    BASE_CHARGE + additional_charge
+  end
+
   private
+
+  def additional_charge
+    case animal.animal_type
+    when 'Dog'
+      hours_requested * DOG_CHARGE
+    when 'Cat'
+      hours_requested * CAT_CHARGE
+    else
+      0
+    end
+  end
 
   def date_of_service_cannot_be_in_the_past
     if date_of_service.present? && date_of_service < Date.today
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :bookings, only: [:new, :create, :index]
   root 'bookings#new' # landing page with the form
   get 'admin', to: 'bookings#index' # admin page
+  post 'bookings', to: 'bookings#create' # create a new booking
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
