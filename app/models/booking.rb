137b0cc51a48b31c287f325e89da0a1d1b7a09b0
Chase <chase.lindsay1@gmail.com>
2024-01-26 22:20:04 -0500

    Added date check to date check method on booking model and added styling to form

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,4 +4,13 @@ class Booking < ApplicationRecord
   accepts_nested_attributes_for :user, :animal
   validates :user, :animal, :hours_requested, :date_of_service, presence: true
   validates :hours_requested, numericality: { only_integer: true, greater_than_or_equal_to: 2, less_than_or_equal_to: 8 }
+  validate :date_of_service_cannot_be_in_the_past
+
+  private
+
+  def date_of_service_cannot_be_in_the_past
+    if date_of_service.present? && date_of_service < Date.today
+      errors.add(:date_of_service, "Date can't be in the past")
+    end
+  end
 end
